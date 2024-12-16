b4f2f996efb75605433ee84758e5b9fecef62b0f
pydidish <pydidish@yandex.ru>
2024-12-16 14:33:08 +0300

    third commit

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+
 created_date = "13-12-2024"  # дата создания заметки в формате "день-месяц-год"
 issue_date = "31-12-2024"  # дата истечения заметки (дедлайн) в формате "день-месяц-год"
 
-print("Дата создания:", created_date[:5])
-print("Дедлайн:", issue_date[:5])
+temp_created_data = datetime.strptime(created_date, "%d-%m-%Y")
+temp_issue_data =  datetime.strptime(issue_date, "%d-%m-%Y")
+
+print("Дата создания:", temp_created_data.strftime("%d-%m"))
+print("Дедлайн:", temp_issue_data.strftime("%d-%m"))
